e9e28252c07253ae3a61d0a4c6d40c3b9fa2f476
Paulo Cesar Dias de Souza <paulosouza90@gmail.com>
2022-08-17 19:42:48 -0300

    adiconado regra de negocio - entrada unica por mes

--- budget/settings.py
+++ budget/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = SECRETS['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
--- budget_api/models.py
+++ budget_api/models.py
@@ -1,10 +1,12 @@
+import django.db.utils
 from django.db import models
 from datetime import date
+from django.http import HttpResponse
 
 
 class Despesa(models.Model):
-    descricao = models.CharField(max_length=255)
-    valor = models.FloatField()
+    descricao = models.CharField(max_length=255, unique_for_month='data')
+    valor = models.DecimalField(max_digits=9, decimal_places=2)
     data = models.DateField(default=date.today)
 
     def __str__(self):
@@ -12,8 +14,8 @@ class Despesa(models.Model):
 
 
 class Receita(models.Model):
-    descricao = models.CharField(max_length=255)
-    valor = models.FloatField()
+    descricao = models.CharField(max_length=255, unique_for_month='data')
+    valor = models.DecimalField(max_digits=9, decimal_places=2)
     data = models.DateField(default=date.today)
 
     def __str__(self):
